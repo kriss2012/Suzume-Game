b72c3969fbac92da85e19f3ccce91bb7bf39c28b
Krishna Patil <179607368+kriss2012@users.noreply.github.com>
2026-06-19 22:48:17 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,9 +15,19 @@ android {
         versionName = "1.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("release.jks")
+            storePassword = "suzume123"
+            keyAlias = "suzume"
+            keyPassword = "suzume123"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
